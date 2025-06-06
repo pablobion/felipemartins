1ca4563001992c6b0db0505e86b68444c382b812
Pablo Bion <pablo.bion@hotmail.com>
2025-06-06 02:08:04 -0300

    gh pages

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/felipemartins',
+  assetPrefix: '/felipemartins',
+};
+
+export default nextConfig; 
